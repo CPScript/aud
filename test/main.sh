e9bd73b31337ef004fa9e8f9bf70a4ad5f1d3951
Disease <83523587+CPScript@users.noreply.github.com>
2024-09-24 10:19:46 -0400

    Update main.sh

--- test/main.sh
+++ test/main.sh
@@ -14,6 +14,22 @@ basic_menu() {
         echo "10. User Account Audit"
         echo "11. Group Policy Audit"
         echo "0. Back"
+
+        read -p "Choose an option [0-11]: " choice 
+
+        case $choice in 
+            1) perform_network_scan ;;
+            2) perform_vulnerability_assessment ;;
+            3) run_compliance_check ;;
+            4) collect_system_info ;;
+            5) check_password_policy ;;
+            6) check_firewall_status ;;
+            7) generate_audit_report ;;
+            8) hardware_inventory_audit ;;
+            9) software_inventory_audit ;;
+            10) user_account_audit ;;
+            11) group_policy_audit ;;
+            0) back ;;
 }
 
 system_menu() {
@@ -25,6 +41,17 @@ system_menu() {
         echo "5. Host System Restore Audit"
         echo "6. Host System Update Audit"
         echo "0. Back"
+
+        read -p "Choose an option [0-6]: " choice 
+
+        case $choice in 
+            1) system_configuration_audit ;;
+            2) system_performance_audit ;;
+            3) system_security_audit ;;
+            4) system_backup_audit ;;
+            5) system_restore_audit ;;
+            6) system_update_audit ;;
+            0) back ;;
 }
 
 server_menu() {
@@ -40,6 +67,21 @@ server_menu() {
         echo "9. Server Restore Audit"
         echo "10. Server Update Audit"
         echo "0. Back"
+
+        read -p "Choose an option [0-10]: " choice 
+
+        case $choice in 
+            1) server_hardware_inventory_audit ;;
+            2) server_software_inventory_audit ;;
+            3) server_user_account_audit ;;
+            4) server_group_policy_audit ;;
+            5) server_configuration_audit ;;
+            6) server_performance_audit ;;
+            7) server_security_audit ;;
+            8) server_backup_audit ;;
+            9) server_restore_audit ;;
+            10) server_update_audit ;;
+            0) back ;;
 }
 
 network_menu() {
@@ -55,21 +97,56 @@ network_menu() {
         echo "9. Network Access Audit"
         echo "10. Network Vulnerability Audit"
         echo "0. Back"
+
+        read -p "Choose an option [0-10]: " choice 
+
+        case $choice in 
+            1) network_topology_audit ;;
+            2) network_device_audit ;;
+            3) network_configuration_audit ;;
+            4) network_performance_audit ;;
+            5) network_security_audit ;;
+            6) network_traffic_audit ;;
+            7) network_protocol_audit ;;
+            8) network_service_audit ;;
+            9) network_access_audit ;;
+            10) network_vulnerability_audit ;;
+            0) back ;;
 }
 
 scripting_menu() {
     while true; do
-        echo "38. Script Syntax Audit"
-        echo "39. Script Logic Audit"
-        echo "40. Script Performance Audit"
-        echo "41. Script Security Audit"
-        echo "42. Script Compatibility Audit"
-        echo "43. Script Error Handling Audit"
-        echo "44. Script Logging Audit"
-        echo "45. Script Testing Audit"
-        echo "46. Script Deployment Audit"
-        echo "47. Script Maintenance Audit"
+        echo "1. Script Syntax Audit"
+        echo "2. Script Logic Audit"
+        echo "3. Script Performance Audit"
+        echo "4. Script Security Audit"
+        echo "5. Script Compatibility Audit"
+        echo "6. Script Error Handling Audit"
+        echo "7. Script Logging Audit"
+        echo "8. Script Testing Audit"
+        echo "9. Script Deployment Audit"
+        echo "10. Script Maintenance Audit"
         echo "0. Back"
+
+        read -p "Choose an option [0-10]: " choice 
+
+        case $choice in
+            1) script_syntax_audit ;;
+            2) script_logic_audit ;;
+            3) script_performance_audit ;;
+            4) script_security_audit ;;
+            5) script_compatibility_audit ;;
+            6) script_error_handling_audit ;;
+            7) script_logging_audit ;;
+            8) script_testing_audit ;;
+            9) script_deployment_audit ;;
+            10) script_maintenance_audit ;;
+            0) back ;;
+
+}
+
+policys_check() {
+ echo "hello, world"
 }
 
 main_menu() {
@@ -78,82 +155,27 @@ main_menu() {
         echo "2. Basic Audit Tools"
         echo "3. Host system Audit Tools"
         echo "4. Server Audit Tools"
-        echo "4. Network Audit Toolls"
-        echo "5. Scripting Audit Tools"
-        echo "6. Check Policys (system policys & repo licence)"
-        
+        echo "5. Network Audit Toolls"
+        echo "6. Scripting Audit Tools"
+        echo "7. Check Policys (system policys & repo licence)"
         echo "0. Exit"
 
-        read -p "Choose an option [0-5]: " choice # add to the rest of functions
+        read -p "Choose an option [0-6]: " choice
 
-        case $choice in # need to organize to functions!
-            1) perform_network_scan ;;
-            2) perform_vulnerability_assessment ;;
-            3) run_compliance_check ;;
-            4) collect_system_info ;;
-            5) check_password_policy ;;
-            6) check_firewall_status ;;
-            7) generate_audit_report ;;
-            8) hardware_inventory_audit ;;
-            9) software_inventory_audit ;;
-            10) user_account_audit ;;
-            11) group_policy_audit ;;
-            12) system_configuration_audit ;;
-            13) system_performance_audit ;;
-            14) system_security_audit ;;
-            15) system_backup_audit ;;
-            16) system_restore_audit ;;
-            17) system_update_audit ;;
-            18) server_hardware_inventory_audit ;;
-            19) server_software_inventory_audit ;;
-            20) server_user_account_audit ;;
-            21) server_group_policy_audit ;;
-            22) server_configuration_audit ;;
-            23) server_performance_audit ;;
-            24) server_security_audit ;;
-            25) server_backup_audit ;;
-            26) server_restore_audit ;;
-            27) server_update_audit ;;
-            28) network_topology_audit ;;
-            29) network_device_audit ;;
-            30) network_configuration_audit ;;
-            31) network_performance_audit ;;
-            32) network_security_audit ;;
-            33) network_traffic_audit ;;
-            34) network_protocol_audit ;;
-            35) network_service_audit ;;
-            36) network_access_audit ;;
-            37) network_vulnerability_audit ;;
-            38) script_syntax_audit ;;
-            39) script_logic_audit ;;
-            40) script_performance_audit ;;
-            41) script_security_audit ;;
-            42) script_compatibility_audit ;;
-            43) script_error_handling_audit ;;
-            44) script_logging_audit ;;
-            45) script_testing_audit ;;
-            46) script_deployment_audit ;;
-            47) script_maintenance_audit ;;
-            48) exit 0 ;;
+        case $choice in
+            1) check_root ;;
+            2) basic_menu ;;
+            3) system_menu ;;
+            4) server_menu ;;
+            5) network_menu ;;
+            6) scripting_menu
+            7) policys_check ;;
+            0) exit 0 ;;
             *) echo "Invalid choice. Try again." ;;
         esac
     done
 }
 
-# New audit functions
-hardware_inventory_audit() {
-    # TO DO: implement hardware inventory audit logic
-    echo "Hardware inventory audit in progress..."
-}
+# <New audit functions go here>
 
-software_inventory_audit() {
-    # TO DO: implement software inventory audit logic
-    echo "Software inventory audit in progress..."
-}
-
-user_account_audit() {
-    # TO DO: implement user account audit logic
-    echo "User account audit in progress..."
-}
-
-group_policy_audit() {
+main_menu
