35a5c3250728e45ca5082988dd37059512d27cbc
Disease <83523587+CPScript@users.noreply.github.com>
2024-09-24 10:41:18 -0400

    Create root_check.sh

---
+++ test/lib/root_check.sh
@@ -0,0 +1,14 @@
+# taken from another script
+root_check() {
+  echo "Checking..."
+  sleep 1
+  clear
+  
+  if [ "$(id -u)" != "0" ]; then
+      echo "You must be rooted."
+      echo "Please check your operating systems documentation to learn how to root your device"
+      exit 1
+  fi
+  echo "Rooted, Thank you for such. Continuing"
+  sleep 1
+}
