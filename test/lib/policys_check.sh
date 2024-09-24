08d75089cf170d59a7ab5c1dbce621d2a2045bed
Disease <83523587+CPScript@users.noreply.github.com>
2024-09-24 12:22:05 -0400

    Update policys_check.sh

--- test/lib/policys_check.sh
+++ test/lib/policys_check.sh
@@ -1,1 +1,15 @@
-<imma make a function to deleate the script if the origonal licence in edited or removed>
+#!/bin/bash
+
+cd; cd system-audit
+DIR="/src"
+FILE_PATH="./licence.txt"
+
+if [ -f "$FILE_PATH" ]; then
+  echo "SYSTEM-AUDIT's LICENCE"
+  cat "$FILE_PATH"
+else
+  echo "Licence not found: $FILE_PATH"
+  echo "DO NOT EDIT THE LICENCE"
+  rm -f "$DIR"/*
+
+fi
